863c35ae0258a75b7214caf932d92c8b9007211a
Apprentice <apprentice@dbc-28.local>
2013-11-11 10:50:19 -0800

    Refactored update_player_position to take in player track

--- jsracer.js
+++ jsracer.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
 
   $(document).on('keyup', function(event) {
+
     if (event.which == 65) {
-      update_player_position()
+      update_player_position("player1_track")
     }
+
+    if (event.which == 76) {
+      update_player_position("player2_track")
+    }
+
   })
 
-  var update_player_position = function(){
-    $(".player1_track .active").removeClass("active").next().addClass("active")
+  var update_player_position = function(player_track){
+    $("." + player_track + " .active").removeClass("active").next().addClass("active")
   }
 
 
