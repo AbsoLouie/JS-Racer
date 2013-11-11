86cf5140c0bdc5de571650dd8d0164fe36bc45d3
Apprentice <apprentice@dbc-28.local>
2013-11-11 10:43:01 -0800

    Added keyup event listener

---
+++ jsracer.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  $(document).on('keyup', function(event) {
+    if (event.which == 65) {
+      console.log("The 'A' key was pressed")
+    }
+  })
+
+
+
+  var update_player_position = function(){
+
+  }
+
+
+
+}) //end of ready
